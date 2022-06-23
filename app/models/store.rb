bdbc470d86dbf1232fe4216d25d6b98a3c799fee
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-23 21:59:30 +0900

    ActiveHash 導入

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,4 +3,11 @@ class Store < ApplicationRecord
   has_one_attached :image
   # has_many :comments
 
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
 end
