eb5a7b3aaf757b95151497c58e201668493a11f0
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-25 12:30:50 +0900

    Storesコントローラー　アクション記述　投稿ビューファイル記述

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -5,9 +5,16 @@ class StoresController < ApplicationController
   end
 
   def new
+    @store = Store.new
   end
 
   def create
+    @store = Store.new(store_params)
+    if @store.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
