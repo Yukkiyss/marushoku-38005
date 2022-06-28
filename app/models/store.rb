b73a3541aa9cfc5355b51fee7c815104eea008c4
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-28 16:06:13 +0900

    Storeモデル単体テスト　記述

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,6 +3,11 @@ class Store < ApplicationRecord
   has_one_attached :image
   # has_many :comments
 
+  validates :store_name, presence: true
+  validates :store_environment, presence: true
+  validates :recommendation, presence: true
+  validates :image, presence: true
+
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
   end
--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :store do
+    store_name             { '子猫' }
+    category_id           { 2 }
+    store_environment     { '子供用の椅子があります' }
+    recommendation           { 'ランチについてくるシフォンケーキが美味しいです。' }
+    association :user
+
+    after(:build) do |store|
+      store.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
     
   end
 end
